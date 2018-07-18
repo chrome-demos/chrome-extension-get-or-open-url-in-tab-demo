212d19edfe84b492f051b6d48aeebd07a87f9b13
freewind <nowindlee@gmail.com>
2018-07-18 11:51:12 +0800

    demo

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-Chrome Extension Using KotlinJs
-================================
+Chrome Extension Get Or Open specified Url in Tab Demo
+======================================================
 
-A simple chrome extension to show current url, written with KotlinJs.
+Check if specified url is opened in tab, select it or open it in new tab.
+
+Notice
+------
+
+- We have to include `tabs` as `permissions` in `manifest.json`, otherwise the url of tabs we get are always `underfined`.
+- `*://*.taobao.com` is not valid url pattern, but `*://*.taobao.com/` or `*://*.taobao.com/*` is
+- `<all_urls>` is for all urls
 
 Build:
+-----
 
 ```
 ./gradlew compileKotlin2Js
@@ -12,7 +20,13 @@ npm install
 ```
 
 Install:
+--------
 
 Chrome -> `Extensions` -> Enable `Developer Mode` -> `Load unpacked` -> select the `extension` directory of this project.
 
 Open a page, and click the icon of this extension on toolbar, you will see a popup dialog with current url.
+
+Resource
+--------
+
+- url pattern: <https://developer.chrome.com/extensions/match_patterns>
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,25 +1,28 @@
 package example
 
 import JsObj
+import chrome.tabs.CreateProperties
 import chrome.tabs.QueryInfo
-import kotlin.browser.document
+import chrome.tabs.UpdateProperties
 import kotlin.browser.window
 
 fun main(args: Array<String>) {
-    document.addEventListener("DOMContentLoaded", {
-        getCurrentTabUrl { url ->
-            window.alert("current url is: $url")
+    chrome.tabs.query(JsObj<QueryInfo>().apply {
+        this.url = "*://*.taobao.com/*"
+    }) { tabs ->
+        if (tabs.isEmpty()) {
+            openNewTab()
+        } else {
+            window.alert("Found one taobao tab, select it now")
+            chrome.tabs.update(tabs.first().id!!, JsObj<UpdateProperties>().apply {
+                this.highlighted = true
+            })
         }
+    }
+}
+
+private fun openNewTab() {
+    chrome.tabs.create(JsObj<CreateProperties>().apply {
+        this.url = "https://taobao.com"
     })
 }
-
-private fun getCurrentTabUrl(callback: (String) -> Unit) {
-    val queryInfo = JsObj<QueryInfo>().apply {
-        active = true
-        currentWindow = true
-    }
-    chrome.tabs.query(queryInfo) { tabs ->
-        tabs.firstOrNull()?.url?.run(callback)
-    }
-}
-
